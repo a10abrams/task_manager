2b338018df92e2cb356b528ffbc3aa5474ba1779
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-01-06 15:53:54 -0500

    Upload index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Calendar from './components/calendar'
+import * as dayjs from 'dayjs'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Task Manager</title>
+        <meta name="description" content="Oragnize your daily, weekly, monthly tasks" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${styles.main} ${inter.className}`}>
+        <Calendar/>
+        <div className={styles.description}>
+        </div>
+      </main>
+    </>
+  )
+}
