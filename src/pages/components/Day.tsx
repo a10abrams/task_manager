5d903593a0051070a2f849935752f31850a16cf1
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-01-26 16:17:29 -0500

    Create Day.tsx
    - moved `Day` component here
    -Modify content variable; better for UI

---
+++ src/pages/components/Day.tsx
@@ -0,0 +1,66 @@
+import { constants } from 'perf_hooks';
+import React from 'react';
+
+interface DayProps {
+  date: string;
+  dayOfMonth: number;
+  isCurrentMonth: boolean;
+  today: string;
+  events: any[];
+  observancesData: any[];
+  allYearDays: any[];
+}
+
+// Map event ID's to unicode symbols
+const unicodeSymbolsMap = {
+  newYears: '\u{2739}', // 12-pointed star
+  mlkDay : '\u{1F54A}', // white dove
+  washBirth : '\u{D80C}\u{DF0F}', //hatchet
+  valDay : '\u{2661}', // heart
+  earthDay : '\u{2672}', // recycle symbol
+  memDay : '\u{2698}', // flower
+  june19 : '\u{25A0}', // black square
+  july4 : '\u{2691}', //flag
+  labDay : '\u{2692}', // hammer + pick
+  secondMon : '\u{13185}', //feather
+  vetDay : '\u{272F}', // pinwheel star
+  thxDay : '\u{270C}', // peace fingers
+  halloween : '\u{2620}', // skull and cross bones
+  christmas : '\u{2603}', // happy snowman
+}
+
+const Day: React.FC<DayProps> = ({ date, dayOfMonth, isCurrentMonth, today, events, observancesData, allYearDays }) => {
+  // Check if the day has any user-created events
+  const hasUserEvents = events.length > 0;
+
+  // Check if the day is in observancesData
+  const isObservance = observancesData.some((observance) => observance.date === date);
+
+  // Get the id of the observance
+  const observanceId = isObservance ? observancesData.find((observance) => observance.date === date).id : '';
+
+  // Determine content based on the conditions
+  let content;
+  if (hasUserEvents && isObservance && unicodeSymbolsMap[observanceId]) {
+    content = `${dayOfMonth} ${unicodeSymbolsMap[observanceId]} *`;// '*' for user-created events
+  } else if (hasUserEvents) {
+    content = `${dayOfMonth} *`;
+  } else if (isObservance && unicodeSymbolsMap[observanceId]) {
+    content = `${dayOfMonth} ${unicodeSymbolsMap[observanceId]}`;
+  } else {
+    content = dayOfMonth.toString();
+  }
+
+  // Set CSS classes based on conditions
+  const dayClasses = `day_grid ${!isCurrentMonth ? "not_current_day" : ""} ${
+    date === today ? "today_calendar_day" : ""
+  } ${observanceId ? `observance_${observanceId}` : ''}`;
+
+  return (
+    <li className={dayClasses} data-date={date}>
+      <span dangerouslySetInnerHTML={{ __html: content }}></span>
+    </li>
+  );
+};
+
+export default Day;
