c954d53dab5c9271a77043d86ac0937fac5ff61e
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-02 16:44:13 -0500

    Update eventsForm.tsx
    - Fixed event indicator showing at the wrong times and functionality
    - Defined prop types ('React.FC' syntax)

--- src/pages/components/eventsForm.tsx
+++ src/pages/components/eventsForm.tsx
@@ -2,7 +2,13 @@ import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs'
 import { getEventsFromLocalStorage } from '../utilities/localStorageUtils';
 
-const EventsForm = () => {
+// Define prop type
+interface EventsFormProps {
+  updateCalendar: () => void;
+}
+
+// EventsForm is specified as a functional component (FC), `EventsFormProps` is the generic type
+const EventsForm: React.FC<EventsFormProps> = ({ updateCalendar }) => {
   const [name, setName] = useState('');
   const [date, setDate] = useState('');
   const [description, setDescription] = useState('');
@@ -56,12 +62,12 @@ const EventsForm = () => {
       description,
     };
 
-    // Update the events state with the new event
+    // Update the events state with the new event + trigger calendar update
     setEvents((prevEvents) => {
       const updatedEvents = [...prevEvents, newEvent];
 
       // Save events to localStorage
-      saveEventsToLocalStorage(updatedEvents);
+      saveEventsToLocalStorage(updatedEvents, updateCalendar);
       
       return updatedEvents;
     });
@@ -74,13 +80,44 @@ const EventsForm = () => {
 
   // This simplified (no more stringify) function directly recieves the events array
   // Now, it should ensure that the data being saved to localStorage is serializable--name, date, and description
-  const saveEventsToLocalStorage = (events) => {
+  const saveEventsToLocalStorage = (events: Event[], updateCalendar: () => void) => {
     // Extract only necessary properties
     const serializableEvents = events.map(({ name, date, description }) => ({ name, date, description }));
   
     // Save events to local storage
     localStorage.setItem("eventsData", JSON.stringify(serializableEvents));
+
+    // Trigger callback to update calendar
+    if (updateCalendar) {
+      updateCalendar();
+    }
   };
+
+  // Delete events
+  const handleDelete = (index) => {
+    // Remove event from the list
+    const updatedEvents = [...events.slice(0, index), ...events.slice(index + 1)];
+    setEvents(updatedEvents);
+
+    // Update localstorage and trigger calendar update
+    saveEventsToLocalStorage(updatedEvents, updateCalendar);
+  }
+
+  // Edit events
+  const handleEdits = (index) => {
+    const editedEvent = events[index];
+    // Set form fields with the values of the event to be edited
+    setName(editedEvent.name);
+    setDate(dayjs(editedEvent.date).format("MM-DD"));
+    setDescription(editedEvent.description);
+
+    // Remove the event from the list
+    const updatedEvents = [...events.slice(0, index), ...events.slice(index + 1)];
+    setEvents(updatedEvents);
+
+    // Update localstorage and trigger calendar update
+    saveEventsToLocalStorage(updatedEvents, updateCalendar);
+  }
 
   return (
     <>
@@ -91,18 +128,22 @@ const EventsForm = () => {
             <table>
               <thead>
                 <tr>
-                  <th>Name</th>
-                  <th>Date</th>
-                  <th>Description</th>
+                  <th className="label_task_table">Name</th>
+                  <th className="label_task_table">Date</th>
+                  <th className="label_task_table">Description</th>
                 </tr>
               </thead>
               <tbody>
                 {/* Display events from localStorage as table rows */}
                 {events.map((event, index) => (
                   <tr key={index}>
-                    <td>{event.name}</td>
-                    <td>{event.date}</td>
-                    <td>{event.description}</td>
+                    <td className="name_table_data">{event.name}</td>
+                    <td className="date_table_data">{dayjs(event.date).format("MM/DD")}</td>
+                    <td className="description_table_data">{event.description}</td>
+                    <td>
+                      <button onClick={() => handleEdits(index)}>*pencil emoji*</button>
+                      <button onClick={() => handleDelete(index)}>X</button>
+                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -111,17 +152,17 @@ const EventsForm = () => {
           <div className="input_form_container">
             <div id="input_form">
               <div className="form_field_container">
-                <label htmlFor="Name">Task name:</label>
+                <label htmlFor="Name">Task name: </label>
                 <input type="text" id="name_field" value={name} onChange={handleNameChange} minLength={3} required />
               </div>
 
               <div className="form_field_container">
-                <label htmlFor="Date">Date:</label>
+                <label htmlFor="Date">Date: </label>
                 <input type="text" id="date_field" placeholder="MM-DD" value={date} onChange={handleDateChange} required />
               </div>
 
               <div className="form_field_container">
-                <label htmlFor="Description">Description:</label>
+                <label htmlFor="Description">Description: </label>
                 <textarea id="description_field" value={description} onChange={handleDescriptionChange} maxLength={250} />
               </div>
 
