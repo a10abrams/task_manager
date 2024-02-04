fd7dd4039d491b10b79e82fc7fcda22a055f8d2a
a10abrams <108836278+a10abrams@users.noreply.github.com>
2024-02-04 10:04:19 -0500

    Update localStorageUtils.ts
    - remove unnecessary notes, some formatting

--- src/pages/utilities/localStorageUtils.ts
+++ src/pages/utilities/localStorageUtils.ts
@@ -4,13 +4,13 @@ export const getEventsFromLocalStorage = () => {
     // Check if localStorage is available
     if (typeof window !== "undefined" && window.localStorage) {
       const storedEventsDataString = localStorage.getItem("eventsData");
+        
       console.log('Stored events data string:', storedEventsDataString);
+     
       const storedEventsData = storedEventsDataString ? JSON.parse(storedEventsDataString) : [];
   
       console.log("Fetched events from localStorage:", storedEventsData);
       
-      //May need this chunk of code for error handling if the JSON error continues
-      // "try" statement from notebook
       return storedEventsData;
     }
     return [];
